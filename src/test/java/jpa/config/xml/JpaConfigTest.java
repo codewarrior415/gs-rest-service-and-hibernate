7f2f5e2464ed7b3fbf12c56654b552705470cab5
Neil Alarcon <neil.alarcon@rhi.com>
2013-12-20 00:10:32 -0800

    Removed test libraries as a build dependency.,

--- src/test/java/jpa/config/xml/JpaConfigTest.java
+++ src/test/java/jpa/config/xml/JpaConfigTest.java
@@ -5,8 +5,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
